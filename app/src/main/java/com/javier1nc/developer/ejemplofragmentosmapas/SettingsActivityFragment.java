5500497ca3a764c3c1f0add53b2b35e2fc9b88fa
Scott <klyagami82@gmail.com>
2019-02-14 07:14:58 -0600

    Initial commit

---
+++ app/src/main/java/com/javier1nc/developer/ejemplofragmentosmapas/SettingsActivityFragment.java
@@ -0,0 +1,29 @@
+package com.javier1nc.developer.ejemplofragmentosmapas;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsActivityFragment extends Fragment {
+
+
+    public SettingsActivityFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings_activity, container, false);
+    }
+
+}
